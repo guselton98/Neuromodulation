2b39c271a39c5ad82e4507bb6bddc464ab4ba968
guselton98 <gus.elton98@gmail.com>
2023-11-06 11:03:29 +1100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Pre-workshop/main.py

--- Part One/Exercise5.py
+++ Part One/Exercise5.py
@@ -29,14 +29,3 @@ ax.set(title="Plot of cell locations recorded",
 plt.show()
 
 # What do you notice??
-
-
-# 2D Histogram plot
-# xedges = np.linspace(0, 10, num=10)
-# yedges = np.linspace(0, 10, num=10)
-# H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
-# H = H.T
-# fig = plt.figure(figsize=(7, 3))
-# ax = fig.add_subplot(131, title='imshow: square bins')
-# plt.imshow(H, interpolation='nearest', origin='lower',
-#         extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
