1d06e6dd1229da6c1bec86e2dfdf5f6d3ec2c73e
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2023-11-11 15:06:50 +1100

    Updated Exercises

---
+++ Part Two/Ex1.py
@@ -0,0 +1,45 @@
+# NEUROMODULATION WORKSHOP
+# PART 2 - EXERCISE 1: DISPLAY AND SEPARATE AN RGB IMAGE
+
+#----------------------------------------------------------------------------------------------------------------------
+# IMPORTING LIBRARIES
+import matplotlib.pyplot as plt
+
+#----------------------------------------------------------------------------------------------------------------------
+# READ IN AN IMAGE FILE
+filename = 'NLE_s1_contra_GFAP-FITC_NeuN-CY5_20x_1.jpg'
+image = plt.imread(filename)
+
+#----------------------------------------------------------------------------------------------------------------------
+# CREATING AND DISPLAYING THE FIGURE
+plt.imshow(image)
+plt.title(filename)
+
+#----------------------------------------------------------------------------------------------------------------------
+# SPLIT THE RGB CHANNELS
+
+red_image = image[:, :, 0]      #R
+green_image = image[:, :, 1]    #G
+blue_image = image[:, :, 2]     #B
+
+#----------------------------------------------------------------------------------------------------------------------
+# CREATING AND DISPLAYING THE FIGURE WITH COLOUR MAPPING
+
+plt.figure(figsize=(12, 3))
+
+plt.subplot(131)
+plt.imshow(red_image, cmap='Reds')
+plt.title("Red Channel")
+
+plt.subplot(132)
+plt.imshow(green_image, cmap='Greens')
+plt.title("Green Channel")
+
+plt.subplot(133)
+plt.imshow(blue_image, cmap='Blues')
+plt.title("Blue Channel")
+
+#----------------------------------------------------------------------------------------------------------------------
+# SHOW ALL OF THE FIGURES
+
+plt.show()
---
+++ Part Two/Ex2.py
@@ -0,0 +1,44 @@
+# NEUROMODULATION WORKSHOP
+# PART 2 - EXERCISE 2: USE IMAGE PROCESSING TO REDUCE NOISE
+
+#----------------------------------------------------------------------------------------------------------------------
+# IMPORTING LIBRARIES
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+#----------------------------------------------------------------------------------------------------------------------
+# READ IN AN IMAGE FILE
+filename = 'NLE_s1_contra_GFAP-FITC_NeuN-CY5_20x_1.jpg'
+image = plt.imread(filename)
+
+#----------------------------------------------------------------------------------------------------------------------
+# SPLIT THE RGB CHANNELS
+
+red_image = image[:, :, 0]      #R
+green_image = image[:, :, 1]    #G
+blue_image = image[:, :, 2]     #B
+
+#----------------------------------------------------------------------------------------------------------------------
+# SHARPEN THE RED CHANNEL
+
+kernel_sharpen_1 = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+sharpened_image = cv2.filter2D(red_image, -1, kernel_sharpen_1)
+
+#----------------------------------------------------------------------------------------------------------------------
+# CREATING AND DISPLAYING THE UNSHARPENED AND SHARPENED FIGURES
+
+plt.figure(figsize=(12, 3))
+
+plt.subplot(121)
+plt.imshow(red_image)
+plt.title("Unsharpened Red Channel")
+
+plt.subplot(122)
+plt.imshow(sharpened_image)
+plt.title("Sharpened Red Channel")
+
+#----------------------------------------------------------------------------------------------------------------------
+# SHOW ALL OF THE FIGURES
+
+plt.show()
